c7f9e4f76d6cc38d1db32c75c368958d95bd3e49
snyt45 <snyt45@gmail.com>
2021-05-02 01:38:09 +0900

    Update initialize.ps1

--- setup/initialize.ps1
+++ setup/initialize.ps1
@@ -1,2 +1,2 @@
-. windows10-settings.ps1
-. install-development-tools.ps1
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/snyt45/windows10-dotfiles/master/setup/windows10-settings.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/snyt45/windows10-dotfiles/master/setup/install-development-tools.ps1'))
