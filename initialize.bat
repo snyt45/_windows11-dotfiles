b4d4f1a566acd9e8c4c7e04ede705c2128b19be7
snyt45 <snyt45@gmail.com>
2021-05-02 02:23:38 +0900

    Delete initialize.bat

--- initialize.bat
+++
@@ -1,2 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy RemoteSigned -File .\setup\windows10-settings.ps1"
-@powershell -NoProfile -ExecutionPolicy RemoteSigned -File .\setup\install-development-tools.ps1"
