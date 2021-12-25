fe1f0f0814277bc76299360b1a014aeac6054aac
snyt45 <snyt45@gmail.com>
2021-12-25 22:33:46 +0900

    Update install-development-tools.ps1

--- setup/install-development-tools.ps1
+++ setup/install-development-tools.ps1
@@ -19,11 +19,11 @@ scoop install git
 scoop bucket add extras
 scoop bucket add nonportable
 scoop bucket add nerd-fonts
-scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
+# scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
 
 # Google Chrome
-Write-Host "Install Google Chrome"
-scoop install googlechrome
+# Write-Host "Install Google Chrome"
+# scoop install googlechrome
 
 # Firefox
 Write-Host "Install Firefox"
@@ -46,36 +46,36 @@ Write-Host "Install Obsidian"
 scoop install obsidian
 
 # PowerToys
-Write-Host "Install PowerToys"
-scoop install powertoys-np
+# Write-Host "Install PowerToys"
+# scoop install powertoys-np
 
 # Windows Terminal
-Write-Host "Install Windows Terminal"
-scoop install windows-terminal 
+# Write-Host "Install Windows Terminal"
+# scoop install windows-terminal 
 
 # PowerShell Core
-Write-Host "Install PowerShell Core"
-scoop install pwsh
+# Write-Host "Install PowerShell Core"
+# scoop install pwsh
 
 # Visual Studio Code
 Write-Host "Install Visual Studio Code"
 scoop install vscode
 
 # PuTTY
-Write-Host "Install PuTTY"
-scoop install PuTTY
+# Write-Host "Install PuTTY"
+# scoop install PuTTY
 
 # Tera Term
-Write-Host "Install Tera Term"
-scoop install teraterm
+# Write-Host "Install Tera Term"
+# scoop install teraterm
 
 # Mery
 Write-Host "Install Merry"
 scoop install mery
 
 # Tablacus Explorer
-Write-Host "Install Tablacus Explorer"
-scoop install tablacus-explorer
+# Write-Host "Install Tablacus Explorer"
+# scoop install tablacus-explorer
 
 # DeepL
 Write-Host "Install DeepL"
@@ -86,16 +86,16 @@ Write-Host "Install f.lux"
 scoop install flux
 
 # MySQL Workbench
-Write-Host "Install MySQL Workbench"
-scoop install mysql-workbench
+# Write-Host "Install MySQL Workbench"
+# scoop install mysql-workbench
 
 # Git Kraken
-Write-Host "Install Git Kraken"
-scoop install gitkraken
+# Write-Host "Install Git Kraken"
+# scoop install gitkraken
 
 # Postman
-Write-Host "Install Postman"
-scoop install postman
+# Write-Host "Install Postman"
+# scoop install postman
 
 # LibreOffice
 Write-Host "Install LibreOffice"
